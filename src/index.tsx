52dd51fecae5058736d9b9f08eab2ca853a661c9
jakubmachac <jakub.machac@beafuture.com>
2023-11-22 12:31:10 +0100

    final solution

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import {boxersList, IBoxer} from "./boxers";
 import {IItem, useItems} from "./useItems";
 
-const Item = ({item, onClick}: {item: IItem, onClick: () => void}) => {
+const Item = memo(({item, onClick}: {item: IItem, onClick: (name: string) => void}) => {
     return (
-        <button key={item.name} style={{ width: 200, margin: 10, padding: 10, border: '1px solid #bbb', backgroundColor: item.selected ? 'hsl(200, 100%, 80%)' : '#FAFAFA' , cursor: "pointer", borderRadius: 10, lineHeight: '10px' }} onClick={onClick}>
+        <button key={item.name} style={{ width: 200, margin: 10, padding: 10, border: '1px solid #bbb', backgroundColor: item.selected ? 'hsl(200, 100%, 80%)' : '#FAFAFA' , cursor: "pointer", borderRadius: 10, lineHeight: '10px' }} onClick={() => onClick(item.name)}>
             <div style={{ fontWeight: "bold", padding: '0 0 6px'}}>{item.name}</div>
             <div style={{ padding: '0 0 8px'}}>({item.division})</div>
             <div style={{ display: "flex", gap: 10, justifyContent: 'space-around', padding: '4px' }}>
@@ -18,7 +18,7 @@ const Item = ({item, onClick}: {item: IItem, onClick: () => void}) => {
             </div>
         </button>
     )
-};
+})
 
 const SelectedItems = ({items}: {items: IItem[]}) => {
     return <div style={{margin: '8px 10px'}}>Selected: {items.map(item => <span key={item.name} style={{ padding: '4px 8px', margin:'4px', backgroundColor: 'hsl(200, 80%, 94%)', border: '2px solid hsl(200, 100%, 80%)' , borderRadius: 10}}>{item.name}</span>)}</div>
@@ -29,7 +29,7 @@ const App = ({ boxers }: { boxers: IBoxer[] }) => {
     return (
         <div style={{display: 'flex', flexWrap: 'wrap' }}>
             <div><SelectedItems items={selectedItems}/></div>
-            <div>{items.map((boxer) => <Item item={boxer} onClick={() => selectItem(boxer)}/>)}</div>
+            <div>{items.map((boxer) => <Item item={boxer} onClick={selectItem}/>)}</div>
         </div>
     )
 }
--- src/useItems.ts
+++ src/useItems.ts
@@ -1,12 +1,18 @@
 import {IBoxer} from "./boxers";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
 export interface IItem extends IBoxer {
     selected?: boolean;
 }
 export const useItems = (initial: IItem[]) => {
     const [items, setItems] = useState<IItem[]>(initial);
-    const selectItem = (boxer: IItem) => setItems(items.map(x => boxer.name === x.name ? {...x, selected: !x.selected} : x));
+    const selectItem = useCallback((boxerName: string) => {
+        setItems((prevItems) =>
+            prevItems.map((x) =>
+                boxerName === x.name ? { ...x, selected: !x.selected } : x
+            )
+        );
+    }, [setItems]);
     const selectedItems = items.filter(x => x.selected)
     return {items, selectedItems, selectItem};
 }
